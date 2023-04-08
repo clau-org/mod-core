06a58f46230897e6af48cd4808315d159a77c3d7
Francisco Ramos <fro.profesional@gmail.com>
2023-04-08 15:14:21 -0700

    Added service/mod.ts

--- .playground/log/main.ts
+++ .playground/log/main.ts
@@ -1,3 +1,22 @@
+/*  
+
+// HOW TO IMPORT
+
+// import from own module
+import {
+  Logger,
+  LogLevels,
+} from "https://raw.githubusercontent.com/clau-org/mod-core/v0.0.1/log/mod.ts";
+
+// import from main module
+import { LOG } from "https://raw.githubusercontent.com/clau-org/mod-core/v0.0.1/mod.ts";
+const { Logger, LogLevels } = LOG;
+
+// import locally
+import { Logger, LogLevels } from "../../log/mod.ts";
+
+*/
+
 import { Logger, LogLevels } from "../../log/mod.ts";
 
 const logger = new Logger();
