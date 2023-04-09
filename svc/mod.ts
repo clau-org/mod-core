f40d63cec5e28a6dfce9531e488b52cd5a4864f9
Francisco Ramos <fro.profesional@gmail.com>
2023-04-09 03:54:36 -0700

    [patch] added schemas

--- svc/mod.ts
+++ svc/mod.ts
@@ -7,3 +7,10 @@ export {
 
 export { middlewareError } from "./middleware/error.ts";
 export { middlewareRequestData } from "./middleware/request_data.ts";
+
+export {
+  schemaId,
+  schemaIds,
+  schemaPage,
+  schemaUuid,
+} from "./schemas/database.ts";
