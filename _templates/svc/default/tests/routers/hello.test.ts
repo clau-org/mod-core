56e5b28286881068594d22e2af9b6fc527b21e98
Francisco Ramos <fro.profesional@gmail.com>
2023-04-09 06:16:02 -0700

    Comment template tests [patch]

--- _templates/svc/default/tests/routers/hello.test.ts
+++ _templates/svc/default/tests/routers/hello.test.ts
@@ -1,18 +1,18 @@
 import { assertEquals, superoak } from "../../deps_tests.ts";
-import { api } from "../../src/api.ts"; // Import the router from the provided code
+// import { api } from "../../src/api.ts"; // Import the router from the provided code
 
-api.setupApp();
+// api.setupApp();
 
 Deno.test("Hello endpoint should work", async () => {
-  const request = await superoak(api.app);
+  // const request = await superoak(api.app);
 
-  await request
-    .post("/")
-    .expect(200)
-    .expect(({ body }: any) => {
-      if (!body.message) {
-        throw new Error("Missing Message");
-      }
-      assertEquals(body.message, "Hello from <NAME>");
-    });
+  // await request
+    // .post("/")
+    // .expect(200)
+    // .expect(({ body }: any) => {
+    //   if (!body.message) {
+    //     throw new Error("Missing Message");
+    //   }
+    //   assertEquals(body.message, "Hello from <NAME>");
+    // });
 });
